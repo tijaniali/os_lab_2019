191f0c8d81995cc0fe090dc5fb86adea29ae76e4
Tijani Ali <tijaniali@Tijanis-MacBook-Air.local>
2025-10-26 22:59:31 +0300

    lab4

--- lab4/src/process_memory.c
+++ lab4/src/process_memory.c
@@ -1,53 +1,55 @@
-
-/* Program to display address information about the process */
-/* Adapted from Gray, J., program 1.4 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-/* Below is a macro definition */
-#define SHW_ADR(ID, I) (printf("ID %s \t is at virtual address: %8X\n", ID, &I))
+/* show address macro uses %p for pointer printing */
+#define SHW_ADR(ID, I) (printf("ID %s\t is at virtual address: %p\n", ID, (void *)&(I)))
 
-extern int etext, edata, end; /* Global variables for process
-                                 memory */
-
-char *cptr = "This message is output by the function showit()\n"; /* Static */
+/* etext/edata/end may be provided by the linker; provide weak fallbacks if missing */
+extern char etext, edata, end; /* Global symbols for process memory */
+#if defined(__APPLE__)
+/* On macOS these symbols may not be exported; provide weak aliases to avoid link errors */
+__attribute__((weak)) char etext;
+__attribute__((weak)) char edata;
+__attribute__((weak)) char end;
+#endif
+char *cptr = "This message is output by the function showit()\n";
 char buffer1[25];
-int showit(); /* Function prototype */
+void showit(const char *p);
 
-main() {
-  int i = 0; /* Automatic variable */
+int main(void) {
+    int i = 0;
 
-  /* Printing addressing information */
-  printf("\nAddress etext: %8X \n", &etext);
-  printf("Address edata: %8X \n", &edata);
-  printf("Address end  : %8X \n", &end);
+    printf("\nAddress etext: %p\n", (void *)&etext);
+    printf("Address edata: %p\n", (void *)&edata);
+    printf("Address end  : %p\n", (void *)&end);
 
-  SHW_ADR("main", main);
-  SHW_ADR("showit", showit);
-  SHW_ADR("cptr", cptr);
-  SHW_ADR("buffer1", buffer1);
-  SHW_ADR("i", i);
-  strcpy(buffer1, "A demonstration\n");   /* Library function */
-  write(1, buffer1, strlen(buffer1) + 1); /* System call */
-  showit(cptr);
+    SHW_ADR("main", main);
+    SHW_ADR("showit", showit);
+    SHW_ADR("cptr", cptr);
+    SHW_ADR("buffer1", buffer1);
+    SHW_ADR("i", i);
+    strcpy(buffer1, "A demonstration\n");
+    write(1, buffer1, strlen(buffer1));
+    showit(cptr);
 
-} /* end of main function */
+    return 0;
+}
 
-/* A function follows */
-int showit(p) char *p;
+void showit(const char *p)
 {
-  char *buffer2;
-  SHW_ADR("buffer2", buffer2);
-  if ((buffer2 = (char *)malloc((unsigned)(strlen(p) + 1))) != NULL) {
-    printf("Alocated memory at %X\n", buffer2);
-    strcpy(buffer2, p);    /* copy the string */
-    printf("%s", buffer2); /* Didplay the string */
-    free(buffer2);         /* Release location */
-  } else {
-    printf("Allocation error\n");
-    exit(1);
-  }
+    char *buffer2;
+    SHW_ADR("buffer2", buffer2);
+    buffer2 = malloc(strlen(p) + 1);
+    if (buffer2 != NULL) {
+        printf("Allocated memory at %p\n", (void *)buffer2);
+        strcpy(buffer2, p);
+        printf("%s", buffer2);
+        free(buffer2);
+    } else {
+        printf("Allocation error\n");
+        exit(1);
+    }
 }
